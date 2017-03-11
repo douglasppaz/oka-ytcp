34ec43a664589bab09afbc998ab2a64f0112aa5c
Douglas Paz <douglas.paz.net@gmail.com>
2017-03-11 14:53:52 -0300

    Webapp: adicionado Webpack em modo desenvolvimento e produção, em desenvolvimento adicionado BrowerSync

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,21 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const distDevPath = 'dist-dev';
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      { test: /\.pug$/, loader: 'pug-loader' }
+module.exports = env => {
+  const { dev } = env || {};
+
+  let webpackConfig = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, dev ? distDevPath : 'dist')
+    },
+    module: {
+      rules: [
+        { test: /\.pug$/, loader: 'pug-loader' }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.pug',
+        filetype: 'pug'
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.pug',
-      filetype: 'pug'
-    })
-  ]
+  };
+
+  if (dev) {
+    webpackConfig.plugins.push(new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: [distDevPath] }
+    }));
+  }
+
+  return webpackConfig;
 };
