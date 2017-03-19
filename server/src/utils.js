8a27e17444690b3dbd729ffc78b5b565b2b79ebf
Douglas <eu@douglaspaz.com>
2017-03-19 16:58:57 -0300

    Servidor: adicionado getErrorLabel em utils

--- server/src/utils.js
+++ server/src/utils.js
@@ -1,4 +1,21 @@
+import { findKey } from 'lodash';
+
+import errosCode from './errorsCode.json';
+
+
+/**
+ * Retorna HTTP response
+ * @param res
+ * @param object
+ * @constructor
+ */
 export const JsonResponse = (res, object) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(object));
 };
+
+/**
+ * Retorna label do error code
+ * @param code
+ */
+export const getErrorLabel = (code) => findKey(errosCode, v => v == code);
