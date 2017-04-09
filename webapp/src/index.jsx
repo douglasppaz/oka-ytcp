d32cb99959f59479ea72c3c35eb44c99b3520493
Douglas <eu@douglaspaz.com>
2017-04-09 08:55:18 -0300

    webapp: adicionado react-redux e store

--- webapp/src/index.jsx
+++ webapp/src/index.jsx
@@ -1,7 +1,15 @@
 import React from'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+
+import store from './redux/store';
+
+
+window.dispatch = store.dispatch;
 
 ReactDOM.render(
-<h1>Hello, world!</h1>,
+  <Provider store={store}>
+    <h1>Hello, world!</h1>
+  </Provider>,
   document.getElementById('app')
 );
