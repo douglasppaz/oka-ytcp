d354e214b8f1863089e61d79ecf621735f533470
Douglas Paz <douglas.paz.net@gmail.com>
2017-04-10 22:49:48 -0300

    webapp: criado tela para selecionar servidor

--- webapp/src/components/SearchServer/SearchServer.jsx
+++ webapp/src/components/SearchServer/SearchServer.jsx
@@ -5,6 +5,14 @@ import keys from 'lodash-es/keys';
 import range from 'lodash-es/range';
 import request from 'request-promise';
 
+import Card from 'material-ui/Card/Card';
+import CardText from 'material-ui/Card/CardText';
+import LinearProgress from 'material-ui/LinearProgress/LinearProgress';
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+import Subheader from 'material-ui/Subheader/Subheader';
+import CloudDone from 'material-ui/svg-icons/file/cloud-done';
+
 import { SERVER_PORT } from '../../../../constants.json';
 import { name, version } from '../../../../package.json';
 
@@ -63,20 +71,26 @@ class SearchServer extends React.Component {
 
   render() {
     const { addrs } = this.state;
-    const availableAddrs = keys(addrs)
-      .filter((key) => addrs[key] === 1)
-      .map((key) => {
-        const value = addrs[key];
-        return (<div key={key}>{key}: {verboseStatus[value] || value}</div>);
-      });
-    const unknownAddrsCount = keys(addrs)
-      .filter((key) => [-1, -2].includes(addrs[key]))
+    const addrsKeys = keys(addrs);
+    const availableAddrs = addrsKeys
+      .filter(key => addrs[key] === 1);
+    const verifiedAddrsCount = addrsKeys
+      .filter((key) => [0, 1, 2].includes(addrs[key]))
       .length;
+    const addrsKeysCount = addrsKeys.length;
+    const percentage = (verifiedAddrsCount / addrsKeysCount) * 100;
+    const availableAddrsItems = availableAddrs.map(ip => (
+      <ListItem leftIcon={<CloudDone />}>{ip}</ListItem>
+    ));
     return (
-      <div>
-        <p>restantes: {unknownAddrsCount}</p>
-        {availableAddrs}
-      </div>
+      <Card className="container absoluteInCenter">
+        <Subheader>Selecione um servidor</Subheader>
+        <List>{availableAddrsItems}</List>
+        <CardText>
+          <p className="textRight small">{addrsKeysCount} IPs listados e {verifiedAddrsCount} verificados</p>
+        </CardText>
+        <LinearProgress mode="determinate" value={percentage} />
+      </Card>
     );
   }
 }
