bf57187e151c008d6163f902f3a8328423c1963d
Douglas Paz <douglas.paz.net@gmail.com>
2017-04-10 21:55:55 -0300

    server: corrigido carregamento das configurações

--- server/src/config.js
+++ server/src/config.js
@@ -6,43 +6,39 @@ import Queue from 'promise-queue';
 import packageInfo from '../../package.json';
 
 const CONFIG_PATH = './config.json';
-const updateConfigFileQueue = new Queue(1, 9999);
+const updateConfigFileQueue = new Queue(1, Infinity);
+const defaultConfig = {
+  name: packageInfo.name,
+  version: packageInfo.version,
+  videosPath: `${os.homedir()}/OKaVideos`
+};
 
-let currrentConfig = {};
+let currrentConfig = null;
 
-const updateCurrentConfig = () => {
+const loadCurrentConfig = () => {
   currrentConfig = fs.readJsonSync(CONFIG_PATH);
   fs.ensureDirSync(currrentConfig.videosPath);
-  return Promise.resolve();
+  return Promise.resolve(currrentConfig);
 };
 
 const updateConfigFile = (config) => new Promise((resolve, reject) => {
   fs.writeJson(CONFIG_PATH, config, (err) => {
     if (err) return reject(err);
-    return updateCurrentConfig()
-      .then(resolve, reject);
+    return resolve();
   });
 });
 
-export const updateConfig = newConfig => updateConfigFileQueue.add(() => {
-  config = defaults(newConfig, currrentConfig);
-  updateConfigFile(config);
-});
-
-const checkConfig = () => {
-  if (!fs.existsSync(CONFIG_PATH)) {
-    const defaultConfig = {
-      name: packageInfo.name,
-      version: packageInfo.version,
-      videosPath: `${os.homedir()}/OKaVideos`
-    };
-    return updateConfig(defaultConfig);
-  }
+export const updateConfig = newConfig => {
+  currrentConfig = defaults(newConfig, currrentConfig);
+  const currrentConfigCopy = currrentConfig;
+  updateConfigFileQueue.add(() => updateConfigFile(currrentConfigCopy));
   return Promise.resolve(currrentConfig);
 };
 
-export const loadConfig = () => checkConfig()
-  .then(updateCurrentConfig)
-  .then(() => console.log('Configurações Carregadas'));
+export const loadConfig = () => {
+  if (!fs.existsSync(CONFIG_PATH)) return updateConfig(defaultConfig);
+  if (!currrentConfig) return loadCurrentConfig();
+  return Promise.resolve(currrentConfig);
+};
 
 export const current = () => Promise.resolve(currrentConfig);
